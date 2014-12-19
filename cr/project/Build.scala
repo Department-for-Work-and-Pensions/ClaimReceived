8296261983e52824c393ce6bc8b19d9b2438267d
Jorge Migueis <jorge.migueis@valtech.co.uk>
2014-12-19 11:45:05 +0000

    Upgraded Play to 2.3.7

--- cr/project/Build.scala
+++ cr/project/Build.scala
@@ -6,7 +6,7 @@ import play.Play.autoImport._
 object ApplicationBuild extends Build {
 
   val appName         = "cr"
-  val appVersion      = "1.1-SNAPSHOT"
+  val appVersion      = "1.2-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
