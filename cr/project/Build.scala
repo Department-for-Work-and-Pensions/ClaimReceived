ff3c430ca27923abb0e83d943b83124e02d119a4
Peter Whitehead <peter@scrumconnect.com>
2016-03-18 09:45:16 +0000

    CADS-785 : fix artifactory publishing for all components

--- cr/project/Build.scala
+++ cr/project/Build.scala
@@ -42,11 +42,24 @@ object ApplicationBuild extends Build {
 
   var f: Seq[Def.Setting[_]] = Seq(sbt.Keys.fork in Test := false)
 
-  var sOrg:Seq[Def.Setting[_]] = Seq(organization  := "uk.gov.service.carersallowance")
+  var vS: Seq[Def.Setting[_]] = Seq(libraryDependencies ++= appDependencies)
 
-  var vS: Seq[Def.Setting[_]] = Seq(version := appVersion, libraryDependencies ++= appDependencies)
+  var sAppN: Seq[Def.Setting[_]] = Seq(name := appName)
+  var sAppV: Seq[Def.Setting[_]] = Seq(version := appVersion)
+  var sOrg: Seq[Def.Setting[_]] = Seq(organization := "gov.dwp.carers")
 
-  var appSettings: Seq[Def.Setting[_]] =  sV ++ sO ++ sR  ++ vS ++ jO ++sOrg ++ f ++ jacoco.settings
+  val isSnapshotBuild = appVersion.endsWith("-SNAPSHOT")
+  var publ: Seq[Def.Setting[_]] = Seq(
+    publishTo := Some("Artifactory Realm" at "http://build.3cbeta.co.uk:8080/artifactory/repo/"),
+    publishTo <<= version {
+      (v: String) =>
+        if (isSnapshotBuild)
+          Some("snapshots" at "http://build.3cbeta.co.uk:8080/artifactory/libs-snapshot-local")
+        else
+          Some("releases" at "http://build.3cbeta.co.uk:8080/artifactory/libs-release-local")
+    })
+
+  var appSettings: Seq[Def.Setting[_]] =  sV ++ sO ++ sR  ++ vS ++ jO ++sOrg ++ f ++ jacoco.settings ++ sAppN ++ sAppV ++ sOrg ++ publ
 
   val main = Project(appName, file(".")).enablePlugins(play.sbt.PlayScala).settings(appSettings: _*)
 
