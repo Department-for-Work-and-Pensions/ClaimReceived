77b7f8326820b8e00b865a6946677960a328cd4f
Peter Whitehead <peter@scrumconnect.com>
2016-06-08 12:04:56 +0100

    CADS-990 : add primary keys to ingress database

--- cr/app/database/DatabaseClaimService.scala
+++ cr/app/database/DatabaseClaimService.scala
@@ -1,8 +1,11 @@
 package database
 
+import anorm.SqlParser._
+import anorm._
 import play.api.Logger
 import play.api.Play.current
 import play.api.db.DB
+import scala.language.postfixOps
 
 trait DatabaseClaimService {
 
@@ -19,6 +22,30 @@ trait DatabaseClaimService {
       statement.close()
       if (!stored) Logger.error(s"Could not store message with transactionId [${transactionId.getOrElse("no transaction id available - schema validation failure?")}].")
       stored
+  }
+
+  def storeDuplicateMessage(msg: String, transactionId: Option[String] = None, signatureCheck: Int): Boolean = DB.withConnection(DATABASE) {
+    implicit connection =>
+      val insertSql: String = "INSERT INTO carers.duplicatemessages (transaction_id, message, signature_check) VALUES (?,?,?);"
+      val statement = connection.prepareStatement(insertSql)
+      statement.setString(1, transactionId.getOrElse("No trans Id"))
+      statement.setString(2, msg)
+      statement.setInt(3, signatureCheck)
+      val stored = statement.executeUpdate() > 0
+      statement.close()
+      if (!stored) Logger.error(s"Could not store duplicate message with transactionId [${transactionId.getOrElse("no transaction id available - duplicate message failure?")}].")
+      stored
+  }
+
+  def loadOriginalMessage(transactionId: String): String = DB.withConnection(DATABASE){
+    implicit connection =>
+      SQL(
+        """
+        SELECT message
+        FROM carers.inspectionstatus
+        WHERE transaction_id = {transactionId}
+        """
+      ).on("transactionId"->transactionId).as(str("message") single)
   }
 }
 
--- cr/app/inspection/SignatureInspector.scala
+++ cr/app/inspection/SignatureInspector.scala
@@ -1,10 +1,12 @@
 package inspection
 
+import java.sql.SQLException
 import database.{DatabaseClaim, DatabaseClaimService}
 import gov.dwp.carers.xml.signing.XmlSignatureValidator
 import play.api.Logger
 import utils.TransactionIdExtractor
-import scala.xml.NodeSeq
+import scala.util.{Failure, Try, Success}
+import scala.xml.{XML, NodeSeq}
 
 /**
  * Check that the XML received as a valid digital signature.
@@ -12,23 +14,44 @@ import scala.xml.NodeSeq
 trait SignatureInspector {
   def databaseClaimService: DatabaseClaimService
 
+  def checkSignature(msg: NodeSeq, transactionId: String): Boolean = {
+    val currentSig = msg \\ "Signature"
+    //load duplicate message
+    val originalSig = XML.loadString(databaseClaimService.loadOriginalMessage(transactionId)) \\ "Signature"
+    (currentSig == originalSig)
+  }
+
+  def checkSignatureAndSaveDuplicateMessage(msg: NodeSeq, transactionId: Option[String]) = {
+    val rtn = checkSignature(msg, transactionId.get) match {
+      case true => Logger.info(s"Signature check same for duplicate message [transactionId=$transactionId]"); databaseClaimService.storeDuplicateMessage(msg.toString, transactionId, 0); true
+      case false => Logger.info(s"Signature check failed for duplicate message [transactionId=$transactionId]"); databaseClaimService.storeDuplicateMessage(msg.toString, transactionId, 1); false
+    }
+    rtn
+  }
+
   def inspectAndSave(msg: NodeSeq): Boolean = {
-      try {
-        val databaseService: DatabaseClaimService = databaseClaimService
-        val msgString = msg.toString()
-        if (XmlSignatureValidator.validate(msgString)) {
-          databaseService.storeMessage(msgString, TransactionIdExtractor.extractTransactionIdFrom(msg))
-          true
-        } else {
-          Logger.error(s"Signature validator failed for transactionId [${TransactionIdExtractor.extractTransactionIdFrom(msg).get}]")
-          false
-        }
+    try {
+      val msgString = msg.toString()
+      val transactionId = TransactionIdExtractor.extractTransactionIdFrom(msg)
+      val rtn = (XmlSignatureValidator.validate(msgString)) match {
+        case true => storeMessage(msg, transactionId);
+        case false => Logger.error(s"Signature validator failed for transactionId [${transactionId.get}]"); false
       }
-      catch {
-        case e:Exception =>
-          Logger.error(s"Signature validator threw exception for transactionId [${TransactionIdExtractor.extractTransactionIdFrom(msg).get}]", e)
-          false
-      }
+      rtn
+    } catch {
+      case e: Exception =>
+        Logger.error(s"Signature validator threw exception for transactionId [${TransactionIdExtractor.extractTransactionIdFrom(msg).get}]", e)
+        false
+    }
+  }
+
+  def storeMessage(msg: NodeSeq, transactionId: Option[String]): Boolean = {
+    val rtn = Try(databaseClaimService.storeMessage(msg.toString(), transactionId)) match {
+      case Success(t) => Logger.debug(s"Successfully saved transactionId=${transactionId.get} in inspectionstatus table"); true
+      case Failure(e: SQLException) if (e.getSQLState == "23505" || e.getErrorCode == 23505) => checkSignatureAndSaveDuplicateMessage(msg, transactionId)
+      case Failure(e) => Logger.error("Unable to save message in db", e); false
+    }
+    rtn
   }
 }
 
